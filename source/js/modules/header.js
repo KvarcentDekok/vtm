b55436fe13bc31cd27f47426b1f4ec5fb73697fd
Oleg Sadchikov <44229623+KvarcentDekok@users.noreply.github.com>
2023-07-13 18:55:28 +0300

    Продолжает разработку

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,6 +1,27 @@
 import {gsap} from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 
+const ToggleText = {
+    Collapsed: 'Показать навигацию',
+    Expanded: 'Скрыть навигацию'
+}
+
+const headerElement = document.querySelector('.header');
+const toggleButton = headerElement.querySelector('.header__toggle');
+const toggleTextElement = toggleButton.querySelector('span');
+
+function toggleClickHandler() {
+    headerElement.classList.toggle('header--expanded');
+
+    if (headerElement.classList.contains('header--expanded')) {
+        toggleButton.setAttribute('aria-expanded', 'true');
+        toggleTextElement.textContent = ToggleText.Expanded;
+    } else {
+        toggleButton.setAttribute('aria-expanded', 'false');
+        toggleTextElement.textContent = ToggleText.Collapsed;
+    }
+}
+
 gsap.registerPlugin(ScrollTrigger);
 
 function header() {
@@ -8,6 +29,10 @@ function header() {
         start: 'top -223',
         end: 99999,
         toggleClass: {className: 'header--scrolled', targets: '.header'}
+    });
+
+    toggleButton.addEventListener('click', () => {
+        toggleClickHandler();
     });
 }
 
