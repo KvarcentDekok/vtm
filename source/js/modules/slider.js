85beffe6fa5a4ec9402c220b53833a1f4ac3a9fc
Oleg Sadchikov <44229623+KvarcentDekok@users.noreply.github.com>
2023-07-14 16:14:44 +0300

    Продолжает разработку

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -65,7 +65,7 @@ export function initSliderExamples() {
         },
         pagination: getPagination('examples-slider'),
         breakpoints: {
-            800: {
+            1100: {
                 slidesPerView: 2
             }
         }
